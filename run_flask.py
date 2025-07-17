038a760a3480e0cc9e5f0fc873d6c46d267f8795
Raj Gohel <Rajthespaceman@gmail.com>
2025-07-17 19:48:23 +0530

    Updated run_flask.py

--- run_flask.py
+++ run_flask.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from auth.routes import auth_bp
 from auth import db  # ✅ Import your database module
+import os
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Use .env later
@@ -11,4 +12,5 @@ app.register_blueprint(auth_bp)
 db.init_db()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
